8b93d0eed90c612d46dfdc8b5b2bac440ba0d48f
Nando <nando@fargeau.nl>
2025-12-01 13:22:26 +0100

    Day 1-2 pending...

--- days/Day_1.php
+++ days/Day_1.php
@@ -46,6 +46,31 @@ class Day_1 extends Day
 
     public function puzzle_2(array $input): string
     {
-        return '';
+        $value = 50;
+        $zeroCount = 0;
+        foreach ($input as $line) {
+            echo PHP_EOL;
+            echo "Line: $line" . PHP_EOL;
+            echo "Value: $value" . PHP_EOL;
+            $newValue = $value + $this->getValueDirection($line);
+            echo "New Value: $newValue" . PHP_EOL;
+            // 138 / 100 = 1
+            if ($newValue > 99 || $newValue < 0) {
+                echo "Zero's count: " . abs(floor($newValue / 100)) . PHP_EOL;
+                $zeroCount += abs(floor($newValue / 100));
+                if ($newValue < 0 && $value === 0) {
+                    echo "Zero's count: -1" . PHP_EOL;
+                    $zeroCount--;
+                }
+            } elseif ($newValue === 0) {
+                echo "Zero's count: 1" . PHP_EOL;
+                $zeroCount++;
+            }
+            $value = $this->correctNumber($newValue);
+            echo "Corrected Value: $value" . PHP_EOL;
+            echo "Zero Count: $zeroCount" . PHP_EOL;
+            echo PHP_EOL;
+        }
+        return $zeroCount;
     }
 }
